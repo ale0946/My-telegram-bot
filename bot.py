c14755b7d729cfb9162615180e69d6746153db26
ale0946 <alemayehumengesha46@gmail.com>
2026-07-30 22:24:07 +0300

    Refactor send_news function for clarity and structure

--- bot.py
+++ bot.py
@@ -6,31 +6,56 @@ TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = "@yegnaLiverpool"
 
 RSS_URL = "https://www.thisisanfield.com/feed/"
+FILE = "last_news.txt"
+
 
 async def send_news(context):
     news = feedparser.parse(RSS_URL)
 
-    if news.entries:
-        item = news.entries[0]
+    if not news.entries:
+        return
 
-        text = f"""
+    item = news.entries[0]
+
+    title = item.title
+    link = item.link
+
+    old_news = ""
+
+    if os.path.exists(FILE):
+        with open(FILE, "r") as f:
+            old_news = f.read()
+
+    # አስቀድሞ ከተላከ አይድገም
+    if link == old_news:
+        return
+
+    text = f"""
 🔴 LIVERPOOL NEWS
 
-📰 {item.title}
+📰 {title}
 
-🔗 {item.link}
+🔗 {link}
 
 @yegnaLiverpool
 """
 
-        await context.bot.send_message(
-            chat_id=CHANNEL_ID,
-            text=text
-        )
+    await context.bot.send_message(
+        chat_id=CHANNEL_ID,
+        text=text
+    )
+
+    # አዲሱን ዜና አስቀምጥ
+    with open(FILE, "w") as f:
+        f.write(link)
+
 
 app = Application.builder().token(TOKEN).build()
 
-# 5 ደቂቃ ሁሉ አዲስ ዜና ይፈትሻል
-app.job_queue.run_repeating(send_news, interval=300, first=5)
+app.job_queue.run_repeating(
+    send_news,
+    interval=300,
+    first=5
+)
 
 app.run_polling()
