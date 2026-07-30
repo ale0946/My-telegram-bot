f91b06649360ec8baf7f3363f025ac0f4135887b
ale0946 <alemayehumengesha46@gmail.com>
2026-07-30 16:13:36 +0300

    Integrate RSS feed to send Liverpool news
    Replaced the test_post function with send_news to fetch and send news from an RSS feed.

--- bot.py
+++ bot.py
@@ -1,23 +1,35 @@
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+import feedparser
+from telegram.ext import Application
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = "@yegnaLiverpool"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔴 Liverpool News Bot ተነስቷል 🚀")
+RSS_URL = "https://www.thisisanfield.com/feed/"
 
-async def test_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=CHANNEL_ID,
-        text="🔴 Liverpool News Bot በትክክል ተገናኝቷል! 🚀"
-    )
-    await update.message.reply_text("Channel ላይ መልእክት ተልኳል ✅")
+async def send_news(app):
+    news = feedparser.parse(RSS_URL)
+
+    if news.entries:
+        item = news.entries[0]
+
+        text = f"""
+🔴 LIVERPOOL NEWS
+
+📰 {item.title}
+
+🔗 {item.link}
+
+@yegnaLiverpool
+"""
+
+        await app.bot.send_message(
+            chat_id=CHANNEL_ID,
+            text=text
+        )
 
 app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("post", test_post))
+app.job_queue.run_once(send_news, 5)
 
 app.run_polling()
