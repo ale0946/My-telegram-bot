3e9f027da3c2fd84f8cdfed3e3f5fb3d1dc22404
ale0946 <alemayehumengesha46@gmail.com>
2026-07-31 01:17:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
-import requests
-
-API_KEY = os.getenv("FOOTBALL_API_KEY")
 import os
+import requests
 import feedparser
+import asyncio
 from telegram import Bot
 
 TOKEN = os.getenv("BOT_TOKEN")
+API_KEY = os.getenv("FOOTBALL_API_KEY")
+
 CHANNEL_ID = "@yegnaLiverpool"
 
 FILE = "last_news.txt"
@@ -17,7 +18,35 @@ def get_sources():
         return [line.strip() for line in f if line.strip()]
 
 
+def get_live_matches():
+    url = "https://v3.football.api-sports.io/fixtures?live=all"
+
+    headers = {
+        "x-apisports-key": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    matches = []
+
+    for game in data.get("response", []):
+        home = game["teams"]["home"]["name"]
+        away = game["teams"]["away"]["name"]
+
+        if "Liverpool" in home or "Liverpool" in away:
+            home_score = game["goals"]["home"]
+            away_score = game["goals"]["away"]
+
+            matches.append(
+                f"⚽ {home} {home_score}-{away_score} {away}"
+            )
+
+    return matches
+
+
 async def send_news():
+
     old_news = ""
 
     if os.path.exists(FILE):
@@ -40,17 +69,27 @@ async def send_news():
         print("No new news")
         return
 
-    title = latest.title
+    live = get_live_matches()
+
+    if live:
+        live_text = "\n".join(live)
+    else:
+        live_text = "የአሁን የሊቨርፑል ቀጥታ ጨዋታ የለም"
+
 
     text = f"""
 🚨🔴 የሊቨርፑል ዜና
 
-📝 {title}
+📝 {latest.title}
+
+⚽ Live:
+{live_text}
 
 📰 ምንጭ: Liverpool News
 
 📢 @yegnaLiverpool
 """
+
 
     bot = Bot(token=TOKEN)
 
@@ -59,6 +98,7 @@ async def send_news():
         text=text
     )
 
+
     with open(FILE, "w") as f:
         f.write(latest.link)
 
@@ -66,31 +106,4 @@ async def send_news():
 
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(send_news())
-def get_live_matches():
-    url = "https://v3.football.api-sports.io/fixtures?live=all"
-
-    headers = {
-        "x-apisports-key": API_KEY
-    }
-
-    response = requests.get(url, headers=headers)
-
-    data = response.json()
-
-    matches = []
-
-    for game in data.get("response", []):
-        home = game["teams"]["home"]["name"]
-        away = game["teams"]["away"]["name"]
-
-        if "Liverpool" in home or "Liverpool" in away:
-            score_home = game["goals"]["home"]
-            score_away = game["goals"]["away"]
-
-            matches.append(
-                f"⚽ {home} {score_home}-{score_away} {away}"
-            )
-
-    return matches
